afc7b6717ac554d61ba71288e05f285884e1af9f
Yossawat Imjai <undefu5624@gmail.com>
2024-04-24 17:48:34 +0700

    add file

---
+++ Challenge/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="flex justify-center mt-10  ">
+
+    <div class="w-[384px] p-4  shadow-2xl border-2 rounded-xl    ">
+        <div class="flex justify-center m-4">
+            <img class=" w-[312px] h-[208px]" src="https://down-th.img.susercontent.com/file/d41adc3b5ce6d6b0e7c4b6c7b6b661d6" alt="">
+        </div>
+        <div >
+            <h2 class="text-2xl underline mb-5">หมอนอิงโคตรไทย ดึกดำบรรพ์</h2>
+            <p>ใช้กันมาอย่างยาวนานตั้งแต่รุ่นปู่ทวด สมัยที่อพยพมาจากจีนข้ามฝ่าน้ำทะเลมาด้วยเรือสำเภาอันยากลำบาก จนมาพบกับหมอนใบนี้ทำให้มีกำลังใจในการหาเช้ากินค่ำ และทำให้ครอบครัวมีกินมาจนถึงทุกวันนี้ไม่ได้ถ้าไม่มีหมอนใบนี้ๆๆๆๆๆ</p>
+            <div class="flex">
+                <button class="p-1 bg-green-400 text-sm m-2  hover:bg-green-500 active:bg-green-600 active:animate-none animate-spin rounded-md ">สั่งซื้อ</button>
+                <p class="flex items-center text-gray-500">ราคา 500 บาท</p>
+            </div>
+        </div>
+    </div>
+</div>
